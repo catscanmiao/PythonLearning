9db692d48b3bb18ad7baad1b64572e1563ef562d
zhouchao <838459647@qq.com>
2017-09-17 22:05:21 +0800

    ++

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # PythonLearning
+
+笔记
+
+1、当前类或方法不引用某个方法时，可以不import，只需在需要引用时import
+
+如：ship.py内的ai_settings，此处未引用不需要import，但是该方法在alien_invasion.py内引用了，只需在该文件内import即可
+
+2、类中变量的初始化
+
+3、
--- game_functions.py
+++ game_functions.py
@@ -4,6 +4,19 @@
 import sys
 import pygame
 
+def check_keydown_event(event, ship):
+    """响应按键"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_event(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
@@ -11,16 +24,10 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_event(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_event(event, ship)
 
 def update_screen(ai_settings, screen, ship):
     """更新屏幕上的图像，并切到新屏幕"""
