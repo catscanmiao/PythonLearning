9d0c1c758b2fa091d21bf77510893c1224544b55
zhouchao <838459647@qq.com>
2017-09-18 18:03:01 +0800

    添加新文件，数据学习

---
+++ matplotlib/mpl_squares.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# !/usr/bin/env pyhon3
+
+#import matplotlib
+import matplotlib.pyplot as plt
+
+#matplotlib.use("Agg")
+squares = [1, 4, 9, 16, 25]
+plt.plot(squares)
+plt.show()
+
